fac9a0730af966f9f9d8d070d6a6a25f5762d857
Edilson Alzemand <edilson_alz@hotmail.com>
2018-10-26 21:13:10 -0300

    update

--- scripts/sakila oltp/sakila_data_schema.sql
+++ scripts/sakila oltp/sakila_data_schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sakila` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `sakila`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sakila
@@ -761,4 +763,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-19 18:10:24
+-- Dump completed on 2018-10-25  0:26:58
